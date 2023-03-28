3fba2b788d3e8b170721f225b9f420cfeb33fd77
mohini <Mohiniruhani112@gmail.com>
2023-03-28 08:57:50 -0400

    updated code

--- src/declartion.java
+++ src/declartion.java
@@ -1,27 +1,27 @@
 // different ways to print array
 
-//public class declartion {
-//    public static void main(String args[])
-//    {
-//        int a[]={10,20,30,40,50};
-//
-//        for(int b : a);
-//        {
-//            System.out.print(b);
-//        }
-//
-//
-//    }
-//
-//}
 public class declartion {
-    public static void main(String args[]){
+    public static void main(String args[])
+    {
+        int a[]={10,20,30,40,50};
+
+        for(int b : a)
+        {
+            System.out.print(b);
+        }
 
 
-            int a[]={10,20,30,40,50};
-            for(int i=0;i<5;i++)
-            {
-                System.out.print(a[i]+" ");
-            }
-            }
     }
+
+}
+//public class declartion {
+//    public static void main(String args[]){
+//
+//
+//            int a[]={10,20,30,40,50};
+//            for(int i=0;i<5;i++)
+//            {
+//                System.out.print(a[i]+" ");
+//            }
+//            }
+//    }
