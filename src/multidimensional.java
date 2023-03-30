c874c55783c2e1703173bc365dbcd13a42e0fca0
mohini <Mohiniruhani112@gmail.com>
2023-03-30 13:31:34 -0400

    updated

--- src/multidimensional.java
+++ src/multidimensional.java
@@ -21,13 +21,14 @@ public class multidimensional {
 
         }
         System.out.print("Array is :");
-        for(int i =0;i<n;i++)
+        for(int i =0;i<n;i++) // n(row)=2 m(column)=3
         {
             for(int j=0;j<m;j++)
             {
-               System.out.print( a[i][j]+ " ");
+               System.out.print( a[i][j]+ " ");// a[0][0],a[0][1],a[0][2],a[1][0],a[1][1],a[1][2]
             }
-            System.out.println();
+            System.out.println(); // a[0][0],a[0][1],a[0][2]
+                                 //  a[1][0],a[1][1],a[1][2]
         }
 
     }
