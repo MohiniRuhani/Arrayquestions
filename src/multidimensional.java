a76e2f629051f38a8f5cac8b5735ea001e7d8f38
mohini <Mohiniruhani112@gmail.com>
2023-03-30 11:42:33 -0400

    declartion of multidimensional array

---
+++ src/multidimensional.java
@@ -0,0 +1,35 @@
+//declartion of multidimensional
+import java.util.Scanner;
+
+public class multidimensional {
+    public static void main(String args [])
+    {
+        int n,m;
+        System.out.print("Enter the number of rows:");
+        Scanner src=new Scanner(System.in);
+        n=src.nextInt();
+        System.out.print("Enter the number of columns:");
+        m=src.nextInt();
+        int a[][]=new int[n][m];
+        System.out.print("Enter the values of array:");
+        for(int i =0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                a[i][j]=src.nextInt();
+            }
+
+        }
+        System.out.print("Array is :");
+        for(int i =0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+               System.out.print( a[i][j]);
+            }
+
+        }
+    }
+
+
+}
